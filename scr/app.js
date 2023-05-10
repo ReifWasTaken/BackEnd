267dc647cc9eebcd4348e183a61f2ed82dc1ad79
Reif <gregodelgado182@gmail.com>
2023-05-10 20:05:51 -0300

    se finalizo la primer entrega creando midwares de carrito y productos con sus respectivas funciones

---
+++ scr/app.js
@@ -0,0 +1,25 @@
+import express from "express"
+import { productsRouter }  from "./routes/products.router.js"
+import { cartsRouter } from "./routes/carts.router.js";
+
+const app = express();
+const port = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
+
+app.listen(port, () => {
+  console.log(`app listening from http://localhost:${port}/api/products`)
+});
+
+app.get("*", (req, res)=>{
+
+    res.status(404).json({
+      status: "error",
+      msg: "route does not exist",
+      data: {},
+    });
+})
---
+++ scr/routes/carts.router.js
@@ -0,0 +1,77 @@
+import express from "express";
+import cartsManager from "../cartManager.js"
+import classProducts from "../productManager.js";
+const cartsRouter = express.Router();
+const cartManager = new cartsManager();
+const productManager = new classProducts();
+
+
+cartsRouter.post("/", async (req, res)=>{
+    try{
+        const newCreation = await cartManager.newCart();
+        
+        return res.status(201).json({
+            status: "success",
+            msg: "cart created",
+            data: newCreation,
+        });
+    }
+    catch(err){
+        return res.status(400).json({
+            status:"error",
+            msg: "cart can not be created",
+        });
+    }
+})
+
+cartsRouter.get("/:cid", async (req, res)=>{
+    try{
+        const solicitedID = req.params.cid;
+        const cartFound = await cartManager.getCartById(parseInt(solicitedID));
+
+          if(cartFound){
+           
+            return res.status(201).json({
+              status: "success",
+              msg: "Cart updated succesfuly",
+              data: cartFound,
+            })
+          }
+      
+        }
+        catch{
+          return  res.status(404).json({
+            status: "error",
+            msg: "Cart does not exist",
+          });
+        }
+    
+})
+
+cartsRouter.post("/:cid/product/:pid", async (req, res)=>{
+    try{
+        const solicitedCartID = req.params.cid;
+        const cartFound = await cartManager.getCartById(parseInt(solicitedCartID));
+
+        const solicitedProductID = req.params.pid;
+        const productFound = await productManager.getProductById(parseInt(solicitedProductID));
+        
+        if(cartFound && productFound){
+           await cartManager.updateCart(cartFound.id, productFound.id);
+
+            return res.status(201).json({
+                status: "success",
+                msg: "cart updated",
+                data: cartFound,
+            })
+        }
+
+    }
+    catch(err){
+        return  res.status(404).json({
+            status: "error",
+            msg: "error adding the product to the cart",
+          });
+    }
+});
+export {cartsRouter};
